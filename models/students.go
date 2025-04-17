ac0f1b4cc22f15f53df976875d356becc3d62baf
lucasoneves <casluhc@gmail.com>
2025-04-17 19:31:19 -0300

    Regex

--- models/students.go
+++ models/students.go
@@ -8,8 +8,8 @@ import (
 type Student struct {
 	gorm.Model
 	Name string `json:"name" validate:"nonzero"`
-	Rg   string `json:"rg" validate:"len=9"`
-	CPF  string `json:"cpf" validate:"len=11"`
+	Rg   string `json:"rg" validate:"len=9, regexp=^[0-9]*$"`
+	CPF  string `json:"cpf" validate:"len=11, regexp=^[0-9]*$"`
 }
 
 func ValidateStudentData(student *Student) error {
