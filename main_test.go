7d57f7fbfc445446c5882c5d3846e5bdda1163d0
lucasoneves <casluhc@gmail.com>
2025-04-19 23:56:44 -0300

    Testando a listagem de alunos

--- main_test.go
+++ main_test.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lucasoneves/api-go-gin/controllers"
+	"github.com/lucasoneves/api-go-gin/database"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupRouterTest() *gin.Engine {
@@ -22,7 +26,26 @@ func TestStudentsRoute(t *testing.T) {
 	response := httptest.NewRecorder()
 	r.ServeHTTP(response, req)
 
-	if response.Code != http.StatusOK {
-		t.Fatalf("Status error: Value received: %d - Expected: %d", response.Code, http.StatusOK)
-	}
+	assert.Equal(t, http.StatusOK, response.Code, "Should be equal")
+	mockDaResposta := `{"message":"Hello World"}`
+	respostaBody, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+
+}
+
+func TestGetAllStudents(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	r := SetupRouterTest()
+	r.GET("/students", controllers.ShowAllStudents)
+
+	req, _ := http.NewRequest("GET", "/students", nil)
+	response := httptest.NewRecorder()
+
+	r.ServeHTTP(response, req)
+
+	assert.Equal(t, http.StatusOK, response.Code, "Should have the same status code")
+
+	fmt.Println(response.Body)
+
 }
