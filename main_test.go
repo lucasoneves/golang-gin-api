88744ba218cab07854e9e3911e7cc5fd31cc5786
lucasoneves <casluhc@gmail.com>
2025-04-24 11:55:10 -0300

    Test de edição de aluno

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -110,10 +111,36 @@ func TestDeleteStudent(t *testing.T) {
 	r := SetupRouterTest()
 	r.DELETE("/students/:id", controllers.DeleteStudent)
 	pathDeBusca := "/students/" + strconv.Itoa(ID)
+
 	req, _ := http.NewRequest("DELETE", pathDeBusca, nil)
 	response := httptest.NewRecorder()
 
 	r.ServeHTTP(response, req)
 
 	assert.Equal(t, http.StatusOK, response.Code, "Should have the same status code")
+}
+
+func TestUpdateStudent(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupRouterTest()
+	r.PATCH("/students/:id", controllers.EditStudent)
+	pathDeEdicao := "/students/" + strconv.Itoa(ID)
+
+	studentUpdated := models.Student{Name: "Nome do Aluno Teste Atualizado", CPF: "12345678985", Rg: "123456747"}
+	studentJson, _ := json.Marshal(studentUpdated)
+
+	req, _ := http.NewRequest("PATCH", pathDeEdicao, bytes.NewBuffer(studentJson))
+	response := httptest.NewRecorder()
+
+	r.ServeHTTP(response, req)
+
+	var studentResponse models.Student
+	json.Unmarshal(response.Body.Bytes(), &studentResponse)
+
+	assert.Equal(t, "12345678985", studentResponse.CPF)
+	assert.Equal(t, "Nome do Aluno Teste Atualizado", studentResponse.Name)
+	assert.Equal(t, "123456747", studentResponse.Rg)
+
 }
